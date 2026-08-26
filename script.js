4fccab1ce629ed5d8876acf9b3529de3454a5d7c
Aman  patel <amanpateljml06@gmail.com>
2026-08-26 23:53:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1952,12 +1952,14 @@ function showCreateProfile(existing) {
         isEditing ? existing.name : (account ? account.name : "");
 
     document.getElementById("farmerPhone").value =
-    isEditing
-        ? existing.phone
-        : (account ? account.phone : "");
+        isEditing ? existing.phone : "";
 
     document.getElementById("farmerVillage").value =
         isEditing ? existing.village : "";
+
+    document.getElementById("farmerCrops").value =
+        isEditing ? existing.crops : "";
+
 
     saveProfileButton.textContent =
         isEditing ? "💾 Save Changes" : "💾 Create Profile";
